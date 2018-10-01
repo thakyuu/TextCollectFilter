aa9afbc30b618b1493adec37be4db3301f4720d2
thakyuu <>
2018-10-02 01:26:05 +0900

    全発言記録をメインに

--- textCollectFilter.go
+++ textCollectFilter.go
@@ -3,8 +3,7 @@ package main
 import (
 	"os"
 	"flag"
-	"strings"
-	"io/ioutil"
+	"time"
 	"golang.org/x/text/transform"
 	"golang.org/x/text/encoding/japanese"
 )
@@ -12,13 +11,12 @@ import (
 const (
 	Space   = " "
 	NewLine = "\n"
+	Tab = "\t"
 )
-
-
 
 func main() {
 	flag.Parse()
-	var s string = ""
+	s := ""
 	
 	for i := 0; i < flag.NArg(); i++ {
 		if i > 0 {
@@ -27,48 +25,23 @@ func main() {
 		s += flag.Arg(i)
 	}
 
-	if(strings.Contains(s, `誤認識 登録`)) {
-		os.Stdout.WriteString(ConvertShiftJIS(""))
-		writeNeedCollectWords()
-	} else {
-		os.Stdout.WriteString(ConvertShiftJIS(s))
-		writeBeforeWords(s)
-	}
+	writeWords(s)
+	os.Stdout.WriteString(ConvertShiftJIS(s))
 }
 
-func writeBeforeWords(s string) {
-	file, err := os.Create("./tmp_beforewords.txt")
+func writeWords(s string) {
+	file, err := os.OpenFile("./000_talk.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 600)
+	
 	if err != nil {
 		panic(err)
 	}
+	
 	defer file.Close()
 	
-	_, err = file.WriteString(s)
+	_, err = file.WriteString(time.Now().String() + Tab + s + NewLine)
 	if err != nil {
 		panic(err)
 	}
-}
-
-func writeNeedCollectWords() {
-	file, err := os.OpenFile("./000_誤認識リスト.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 600)
-	
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	
-	_, err = file.WriteString(readBeforeWords() + NewLine)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func readBeforeWords() (string) {
-	data, err := ioutil.ReadFile("./tmp_beforewords.txt")
-	if err != nil {
-		panic(err)
-	}
-	return(string(data))
 }
 
 func ConvertShiftJIS(str string) (string) {
